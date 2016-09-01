ed3e2e5b3051dac71caa2e0b0619a2fe536c7c51
danilobreda <danilo@bredas.com.br>
2016-09-01 20:33:29 -0300

    incial

---
+++ Pub/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using uPLibrary.Networking.M2Mqtt;
+
+namespace Pub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                MqttClient mqttclient = new MqttClient("iot.eclipse.org"); 
+
+                string clientId = Guid.NewGuid().ToString();
+                mqttclient.Connect(clientId, "teste", "123");
+            do
+            {
+                Console.ReadLine();
+                mqttclient.Publish("Teste123", GetBytes("mensagem teste"));
+
+            } while (true);
+
+
+            Console.ReadKey();
+            }
+            catch (Exception ex)
+            {                
+                throw ex;
+            }
+        }
+
+        static byte[] GetBytes(string str)
+        {
+            byte[] bytes = new byte[str.Length * sizeof(char)];
+            System.Buffer.BlockCopy(str.ToCharArray(), 0, bytes, 0, bytes.Length);
+            return bytes;
+        }
+    }
+}
---
+++ Sub/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using uPLibrary.Networking.M2Mqtt;
+
+namespace Sub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MqttClient mqttclient = new MqttClient("iot.eclipse.org");
+
+            mqttclient.MqttMsgPublishReceived += mqttclient_MqttMsgPublishReceived;
+
+            string clientId = Guid.NewGuid().ToString();
+            mqttclient.Connect(clientId,"teste","123");
+
+            Console.ReadKey();
+        }
+
+        static void mqttclient_MqttMsgPublishReceived(object sender, uPLibrary.Networking.M2Mqtt.Messages.MqttMsgPublishEventArgs e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
